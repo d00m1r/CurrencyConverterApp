97f2a67a61f46ba2c5f6940218f714ae0d163a5c
d00m1r <miniakhmetovdamir@gmail.com>
2020-06-20 21:43:47 +0600

    add home screen, remove strip on status bar on android

--- App/constants/colors.js
+++ App/constants/colors.js
@@ -1,5 +1,5 @@
 export default {
     text: "#111111",
     border : "#E2E2E2",
-    blue: "4f7d7a",
+    blue: "#4f6d7a",
 };
--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,32 +1,40 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, ScrollView, Linking, Alert } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 import colors from "../constants/colors";
 import { RowItem, RowSeparator } from "../components/RowItem";
 
+const openUrl = (url) => {
+  return Linking.openURL(url).catch(() => {
+    Alert.alert("Sorry, something went wrong.", "Please try again later.");
+  });
+};
+
 export default () => {
   return (
-    <SafeAreaView>
-      <RowItem
-        Press={() => alert("to do!")}
-        text="Themes"
-        icon={<Entypo name="chevron-right" size={20} color={colors.blue} />}
-      />
+    <ScrollView>
+      <SafeAreaView>
+        <RowItem
+          Press={() => openUrl("https://www.google.ru/")}
+          text="Themes"
+          icon={<Entypo name="chevron-right" size={20} color={colors.blue} />}
+        />
 
-      <RowSeparator />
+        <RowSeparator />
 
-      <RowItem
-        Press={() => alert("to do!")}
-        text="Description"
-        icon={<Entypo name="chevron-right" size={20} color={colors.blue} />}
-      />
-      <RowSeparator />
+        <RowItem
+          Press={() => alert("to do!")}
+          text="Description"
+          icon={<Entypo name="chevron-right" size={20} color={colors.blue} />}
+        />
+        <RowSeparator />
 
-      <RowItem
-        Press={() => alert("to do!")}
-        text="Samples"
-        icon={<Entypo name="export" size={20} color={colors.blue} />}
-      />
-    </SafeAreaView>
+        <RowItem
+          Press={() => alert("to do!")}
+          text="Samples"
+          icon={<Entypo name="export" size={20} color={colors.blue} />}
+        />
+      </SafeAreaView>
+    </ScrollView>
   );
 };
